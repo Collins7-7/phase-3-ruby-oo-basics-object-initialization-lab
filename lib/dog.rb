c06b631ff888c51e197cb5df0a91347ebf2b5110
COllins <collinsmuiruri77@gmail.com>
2022-10-20 13:31:16 +0300

    Completed assignment

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+require "pry"
+
+class Dog
+     attr_reader :name
+     attr_reader :breed
+     
+    def initialize(name, breed="Mutt")
+        @name = name
+        @breed = breed
+    end
+end
+
+border = Dog.new("Bosco", "Collie")
+
+puts border.name 
